6ff7ba50314058f7526ecf475302c1d540896649
viktorbo <borisoof13@yandex.ru>
2021-02-17 19:33:15 +0300

    Added JUMP, fixed borders and controls

--- Hello_Pygame.py
+++ Hello_Pygame.py
@@ -7,18 +7,23 @@ pygame.init()
 window = pygame.display.set_mode((800, 600))
 pygame.display.set_caption('HELLO PyGAME!')
 
-x = 50
-y = 50
 
 w = 100
 h = 50
 
+x = (800 - w)//2
+y = 600 - h - 5
+
+
 speed = 5
 
-run = True
+game_run = True
 
-while run:
-    pygame.time.delay(10)
+jump = False
+jumpCount = 10
+
+while game_run:
+    pygame.time.delay(100)
 
     for event in pygame.event.get():
         ic(event)
@@ -31,10 +36,23 @@ while run:
         x -= speed
     if keys[pygame.K_RIGHT] and (x < 800 - w -5):
         x += speed
-    if keys[pygame.K_UP] and y > 5:
-        y -= speed
-    if keys[pygame.K_DOWN] and (y < 600 - h - 5):
-        y += speed
+    if not jump:
+        if keys[pygame.K_UP] and y > 5:
+            y -= speed
+        if keys[pygame.K_DOWN] and (y < 600 - h - 5):
+            y += speed
+        if keys[pygame.K_SPACE]:
+            jump = True
+    else:
+        if jumpCount >= -10:
+            if jumpCount < 0:
+                y += (jumpCount ** 2) / 3
+            else:
+                y -= (jumpCount ** 2) / 3
+            jumpCount -= 1
+        else:
+            jump = False
+            jumpCount = 10
 
     window.fill(BLACK)
     pygame.draw.rect(window, BLUE, (x, y, w, h))
